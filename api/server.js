3632dc00685e503958c41c0973c91a531146c7aa
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-11 18:12:32 +0100

    server running, router created, all endpoints working

--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
-// implement your server here
-// require your posts router and connect it here
+// Imports
+const express = require('express');
+const postsRouter = require('./posts/posts-router')
+
+// Express server instance
+const server = express()
+
+// Middleware
+server.use(express.json())
+server.use('/api/posts', postsRouter)
+
+
+// Exports
+module.exports = server
